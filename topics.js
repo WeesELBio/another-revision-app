70bfbb54227f97b44806146e8170e4a843b43d36
WeesELBio <161979348+WeesELBio@users.noreply.github.com>
2026-06-09 22:56:09 +0100

    Create topics.js

---
+++ topics.js
@@ -0,0 +1,46 @@
+export const topics = [
+    {
+        id: "biology",
+        name: "Biology",
+        children: [
+            {
+                id: "biological_molecules",
+                name: "Biological Molecules",
+                children: [
+                    {
+                        id: "intro_biomolecules",
+                        name: "Introduction to Biological Molecules",
+                        children: []
+                    },
+                    {
+                        id: "carbohydrates",
+                        name: "Carbohydrates",
+                        children: []
+                    },
+                    {
+                        id: "lipids",
+                        name: "Lipids",
+                        children: []
+                    },
+                    {
+                        id: "proteins",
+                        name: "Proteins",
+                        children: []
+                    }
+                ]
+            },
+
+            {
+                id: "nucleic_acids",
+                name: "Nucleic Acids",
+                children: []
+            },
+
+            {
+                id: "cell_structure",
+                name: "Cell Structure",
+                children: []
+            }
+        ]
+    }
+];
